621ea66e0898dbd9d59a5cdf2b48f9aeab6bc00e
Alex W <amwiederman15@gmail.com>
2022-12-03 14:08:12 -0500

    added local storage for user and score

--- highScoresScript.js
+++ highScoresScript.js
@@ -1,12 +1,17 @@
 
 var scoreEl = document.querySelector(".score")
 var submitEl = document.querySelector(".submit")
+
 var scoreLocalT =JSON.parse(localStorage.getItem("scoreLocalTemp"))
 
 scoreEl.textContent = "Your Score is: " + scoreLocalT
 
 submitEl.addEventListener("click", function(event){
-    var name = 
-    localStorage.setItem("scoreLocalTemp", JSON.stringify(timerCount));
+   
+    // Getting entered text in text box
+    var nameEl = document.querySelector(".name").value
+  
+    localStorage.setItem("scoreLocal", JSON.stringify(scoreLocalT));
+    localStorage.setItem("userLocal", JSON.stringify(nameEl));
 
 }) 
