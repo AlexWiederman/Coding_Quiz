f422e7250c451788e59d010a8b0b76309b523277
Alex W <amwiederman15@gmail.com>
2022-12-04 12:21:18 -0500

    almost done. need to figure out displaying high scores one after the other

--- highScoresScript.js
+++ highScoresScript.js
@@ -1,17 +1,69 @@
 
 var scoreEl = document.querySelector(".score")
 var submitEl = document.querySelector(".submit")
+var displayScoresEl = document.querySelector(".displayScores")
+var clearAllEl = document.querySelector(".clearAll")
+var lineBreakEl = document.querySelector(".lineBreak")
 
-var scoreLocalT =JSON.parse(localStorage.getItem("scoreLocalTemp"))
+var scoreNew = JSON.parse(localStorage.getItem("scoreLocalTemp"))
 
-scoreEl.textContent = "Your Score is: " + scoreLocalT
+if (scoreNew == null) {
+    window.location.href = "./index.html"
+}
+// submitEl.disabled = false;
 
-submitEl.addEventListener("click", function(event){
-   
-    // Getting entered text in text box
-    var nameEl = document.querySelector(".name").value
-  
-    localStorage.setItem("scoreLocal", JSON.stringify(scoreLocalT));
-    localStorage.setItem("userLocal", JSON.stringify(nameEl));
 
-}) 
+
+scoreEl.textContent = "Your Score is: " + scoreNew
+
+displayHighScores()
+
+submitEl.addEventListener("click", function (event) {
+    // submitEl.disabled = true;
+    var scoreLocalFunction = JSON.parse(localStorage.getItem("scoreLocal"))
+    var userLocalFunction = JSON.parse(localStorage.getItem("userLocal"))
+// Getting entered text in text box
+    var userNew = document.querySelector(".name").value
+    // Check if any data is returned and if not then skip everything
+    if (scoreLocalFunction && userLocalFunction !== null) {
+        
+        
+        // Checking if variable is an array to avoid error when pushing element to array
+        var scoreArray = Array.isArray(scoreLocalFunction);
+        var userArray = Array.isArray(userLocalFunction);
+       
+            // adding new score to the end of the array
+            scoreLocalFunction.push(scoreNew)
+            // adding new user to the end of the array
+            userLocalFunction.push(userNew)
+           
+        
+    } else {
+        scoreLocalFunction = [scoreNew];
+        userLocalFunction = [userNew];
+    }
+
+    localStorage.setItem("scoreLocal", JSON.stringify(scoreLocalFunction));
+    localStorage.setItem("userLocal", JSON.stringify(userLocalFunction));
+    displayHighScores()
+
+})
+
+function displayHighScores() {
+
+    var score = JSON.parse(localStorage.getItem("scoreLocal"));
+    var user = JSON.parse(localStorage.getItem("userLocal"));
+
+    if (score && user !== null) {
+        for (i = 0; i < score.length; i++) {
+                displayScoresEl.textContent = '\n' + user[i] + " ----" + score[i];
+        }
+    } else {
+        displayScoresEl.textContent = "";
+    }
+}
+
+clearAllEl.addEventListener("click", function (event) {
+    localStorage.clear();
+    displayHighScores();
+})
